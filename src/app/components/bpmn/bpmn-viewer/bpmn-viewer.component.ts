ea7d13e67563ecf73f4a2f243d542cf5a9363553
lechinoix <nicolas.ngomai@gmail.com>
2020-10-22 02:57:10 +0200

    display bpmn

--- src/app/components/bpmn/bpmn-viewer/bpmn-viewer.component.ts
+++ src/app/components/bpmn/bpmn-viewer/bpmn-viewer.component.ts
@@ -1,19 +1,34 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, AfterViewInit, ViewChild } from '@angular/core';
 import { BPMNDiffService } from '../bpmn-diff.service';
+import BpmnViewer from 'bpmn-js';
 
 @Component({
   selector: 'bpmn-viewer',
   templateUrl: './bpmn-viewer.component.html',
   styleUrls: ['./bpmn-viewer.component.scss']
 })
-export class BpmnViewerComponent {
+export class BpmnViewerComponent implements AfterViewInit {
   @Input() side: 'left' | 'right';
+  viewer: any;
 
   constructor(
     public bpmnDiffService: BPMNDiffService,
   ) { }
 
+  ngAfterViewInit() {
+    this.viewer = new BpmnViewer({
+      container: `.${this.side}-viewer`
+    });
+  }
+
   setBpmnValue(value) {
     this.bpmnDiffService.setBPMN(value, this.side);
+    this.updateViewer(value);
+  }
+
+  async updateViewer(bpmn) {
+    const { warnings } = await this.viewer.importXML(bpmn);
+    console.log('success !', warnings);
+    this.viewer.get('canvas').zoom('fit-viewport');
   }
 }
